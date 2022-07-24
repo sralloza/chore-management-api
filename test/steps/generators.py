27859dcfe037ead2de8fd080ced615259ddffa90
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-07-24 17:16:11 +0200

    chore: refactor recreateTenantToken feature

--- test/steps/generators.py
+++ test/steps/generators.py
@@ -9,14 +9,26 @@ from common.utils import *
 @given("there are {chore_types:d} chore types")
 def step_impl(context, chore_types):
     for i in range(chore_types):
-        raw_data = {
-            "id": ascii_uppercase[i],
-            "description": f"description{i+1}",
-        }
-
+        raw_data = {"id": ascii_uppercase[i], "description": f"description{i+1}"}
         context.execute_steps(
             f"""
             When I send a request to the Api resource "createChoreType" with body params
+            {payload_to_table_format(raw_data)}
+            Then the response status code is "200"
+            """
+        )
+
+    context.res = None
+
+
+@given("there is {tenants:d} tenant")
+@given("there are {tenants:d} tenants")
+def step_impl(context, tenants):
+    for i in range(1, tenants + 1):
+        raw_data = {"tenant_id": i, "username": f"tenant{i}"}
+        context.execute_steps(
+            f"""
+            When I send a request to the Api resource "createTenant" with body params
             {payload_to_table_format(raw_data)}
             Then the response status code is "200"
             """
--- test/steps/response.py
+++ test/steps/response.py
@@ -1,8 +1,10 @@
 from json import JSONDecodeError, loads
 from pathlib import Path
 
+import jq
 from behave import *
 from deepdiff import DeepDiff
+from hamcrest import *
 
 from common.response import get_step_body_json, remove_attributes
 
@@ -18,7 +20,11 @@ def step_impl(context):
 @step("the Api response contains the expected data")
 def step_impl(context):
     json_file = None
-    skip_params = [x["skip_param"] for x in context.table] if context.table else []
+    if context.table:
+        context.table.require_columns(["skip_param"])
+        skip_params = [x["skip_param"] for x in context.table]
+    else:
+        skip_params = []
 
     if context.text:
         try:
@@ -45,3 +51,30 @@ def step_impl(context):
 
     diff = DeepDiff(expected_json, actual_json)
     assert not diff, f"JSON response differs: {diff}"
+
+
+@step('"I save the response field "{field}" as "{attr}"')
+def step_impl(context, field, attr):
+    field = "." + field if not field.startswith(".") else field
+    context.logger.debug(f'Saving response field "{field}" as "{attr}"')
+    res_json = context.res.json()
+    value = jq.compile(attr).input(res_json).first()
+    setattr(context, attr, value)
+
+
+@then('the response field "{res_attr}" is different than "{saved_attr}"')
+def step_impl(context, res_attr, saved_attr):
+    res_attr = "." + res_attr if not res_attr.startswith(".") else res_attr
+    res_json = context.res.json()
+    actual = jq.compile(res_attr).input(res_json).first()
+    expected = getattr(context, saved_attr)
+    assert_that(expected, is_not(equal_to(actual)))
+
+
+@then('the response field "{res_attr}" is equal to "{saved_attr}"')
+def step_impl(context, res_attr, saved_attr):
+    res_attr = "." + res_attr if not res_attr.startswith(".") else res_attr
+    res_json = context.res.json()
+    actual = jq.compile(res_attr).input(res_json).first()
+    expected = getattr(context, saved_attr)
+    assert_that(expected, equal_to(actual))
