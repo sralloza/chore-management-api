ed14bf539e2d6506065ef50ce0b02939377cd8ae
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-08-03 11:00:20 +0200

    refactor: api modules

--- test/steps/api.py
+++ test/steps/api.py
@@ -4,12 +4,15 @@ import re
 from datetime import datetime
 from pathlib import Path
 
+import jq
 from behave import *
 from dateutil.parser import parse
 from hamcrest import *
 from jsonschema import FormatChecker, RefResolver, ValidationError, validate
+from toolium.utils.dataset import replace_param
 
-from common.utils import *
+from common.api import send_request
+from common.request import table_to_dict
 
 
 @then('the response status code is "{code:d}"')
@@ -24,12 +27,6 @@ def step_impl(context):
         json.loads(context.res.text)
     except ValueError as exc:
         assert False, f"Response body is not a valid json ({exc})"
-
-
-@step("the {header} header is in the response")
-def step_impl(context, header):
-    msg = f"Header {header} is not present in the response.\nHEADERS: {context.res.headers}"
-    assert context.res.headers.getlist(header), msg
 
 
 @step('the error message contains "{message}"')
@@ -102,3 +99,83 @@ def step_impl(context, ms):
 
     assert actual_ms >= 0, f"Timestamp is {actual_ms:.2f} ms into the future"
     assert actual_ms <= ms, f"Timestamp is {actual_ms:.2f} ms ago"
+
+
+@step("The parameters to filter the request")
+def step_impl(context):
+    context.params = table_to_dict(context.table)
+
+
+@step('I send a request to the Api resource "{resource}"')
+def step_impl(context, resource):
+    send_request(context, resource)
+
+
+@step('I send a request to the Api resource "{resource}" with body params')
+def step_impl(context, resource):
+    payload = table_to_dict(context.table)
+    send_request(context, resource, payload=payload)
+
+
+@step("I send a request to the Api with body params")
+def step_impl(context):
+    payload = table_to_dict(context.table)
+    send_request(context, payload=payload)
+
+
+@step("I send a request to the Api with body")
+def step_impl(context):
+    send_request(context, payload=context.text)
+
+
+@step("I send a request to the Api")
+def step_impl(context):
+    send_request(context)
+
+
+@step('the field "{field}" with string value "{value}"')
+def step_impl(context, field, value):
+    set_field_to_context(context, field, value, to_str=True)
+
+
+@step('the field "{field}" with value "{value}"')
+def set_field_to_context(context, field, value, to_str=False):
+    value = replace_param(value, infer_param_type=not to_str)
+    setattr(context, field, value)
+
+
+@step('the field "{field}" saved as "{attr}"')
+def step_impl(context, field, attr):
+    value = getattr(context, attr)
+    set_field_to_context(context, field, value)
+
+
+@step("the fields")
+def step_impl(context):
+    context.table.require_columns(["field", "value"])
+    for row in context.table:
+        field = row["field"]
+        value = row["value"]
+        to_str = row.get("as_string", "fals").lower() == "true"
+        set_field_to_context(context, field, value, to_str=to_str)
+
+
+@step('the response attribute "{attr}" as string is "{value}"')
+def step_impl(context, attr, value):
+    assert_response_attr(context, attr, value, to_str=True)
+
+
+@step('the response attribute "{attr}" is "{value}"')
+def assert_response_attr(context, attr, value, to_str=False):
+    res_json = context.res.json()
+
+    expected_value = replace_param(value, infer_param_type=not to_str)
+
+    attr = "." + attr if not attr.startswith(".") else attr
+    real_value = jq.compile(attr).input(res_json).first()
+
+    assert_that(
+        real_value,
+        equal_to(expected_value),
+        f"Expected attribute {attr} to be {value}",
+    )
