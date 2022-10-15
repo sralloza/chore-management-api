2fee6b32582f3a6a54da73724d88bc37a1846f97
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-10-15 14:40:49 +0200

    delete java code

--- src/main/java/es/sralloza/choremanagementapi/repositories/db/DBRotationRepository.java
+++
@@ -1,9 +0,0 @@
-package es.sralloza.choremanagementapi.repositories.db;
-
-import es.sralloza.choremanagementapi.models.db.DBRotation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBRotationRepository extends JpaRepository<DBRotation, Long> {
-}
--- src/main/java/es/sralloza/choremanagementapi/repositories/db/DBUsersRepository.java
+++
@@ -1,9 +0,0 @@
-package es.sralloza.choremanagementapi.repositories.db;
-
-import es.sralloza.choremanagementapi.models.db.DBUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBUsersRepository extends JpaRepository<DBUser, Long> {
-}
