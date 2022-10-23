d06d3a3b900acb7c9ef1df096974a298feba5052
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-10-23 16:22:43 +0200

    fix

--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from "crypto";
 import jwt, { Secret } from "jsonwebtoken";
 
-const SECRET_KEY: Secret = "secret";
+const SECRET_KEY: Secret = process.env.APPLICATION_SECRET || "secret";
 
 export const genJWT = (expiresIn: number | string) => {
   const token = jwt.sign({ sub: randomUUID() }, SECRET_KEY, {
