168a88cc90e02e2e5aec5a5903d71202fd57bbda
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-11-06 14:25:38 +0100

    test: 404 operations with parameters must declare 404 response (#72)

--- test/metatests/test_openapi.py
+++ test/metatests/test_openapi.py
@@ -1,21 +1,36 @@
 from behave.model import Feature
 
-from common.openapi import get_request_headers
+from common.openapi import *
 from metatests.constants import *
 from metatests.core import *
 
 
 def test_validate_xcorrelator_in_headers(feature: Feature):
-    operation_id = get_operation_id_by_scenario(feature)
+    operation_id = get_operation_id_by_feature(feature)
     headers = get_request_headers(operation_id=operation_id)
     msg = f"[{operation_id}] X-Correlator header is mandatory"
     assert "X-Correlator" in headers, msg
 
 
 def test_openapi_headers_title_cased(feature: Feature):
-    operation_id = get_operation_id_by_scenario(feature)
+    operation_id = get_operation_id_by_feature(feature)
     headers = get_request_headers(operation_id=operation_id)
     for header in headers:
         expected = "-".join([x.title() for x in header.split("-")])
         msg = f"[{operation_id}] Header {header!r} should be {expected!r}"
         assert header == expected, msg
+
+
+def test_get_operations_with_parameter_404(feature: Feature):
+    operation_id = get_operation_id_by_feature(feature)
+    method = get_method(operation_id)
+    if method != "GET":
+        return
+    parameters = get_parameters(operation_id=operation_id)
+    path_parameters = [x["name"] for x in parameters if x["in"] == "path"]
+    if path_parameters:
+        msg = f"[{operation_id}] 404 response is not declared ({path_parameters})"
+        assert 404 in get_response_codes(operation_id=operation_id), msg
+    else:
+        msg = f"[{operation_id}] 404 response is declared, but has no path parameters"
+        assert 404 not in get_response_codes(operation_id=operation_id), msg
