9a4e9b2ca5d5d69b18a425c1211ffa873634b63f
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-12-04 01:08:12 +0100

    add datetime fields to chores

--- app/models/chore.py
+++ app/models/chore.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, Field
-
+from datetime import datetime
 
 class ChoreCreate(BaseModel):
     chore_type: str = Field(max_length=25)
     done: bool = False
     user_id: str = Field(max_length=40)
     week_id: str = Field(max_length=7)
+    created_at: datetime = Field(default_factory=datetime.now)
+    closed_at: datetime | None = None
 
 
 class Chore(ChoreCreate):
