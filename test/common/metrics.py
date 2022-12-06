9b03237aca34d10db4d76e31413dac0704bb2709
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-12-06 21:04:58 +0100

    test: show log if metric doesn't exist

--- test/common/metrics.py
+++ test/common/metrics.py
@@ -1,5 +1,3 @@
-import warnings
-
 from hamcrest import *
 from prometheus_client.parser import text_string_to_metric_families
 
@@ -7,10 +5,6 @@ from common.api import send_request
 from common.utils import URL
 
 METRICS_URL = URL + "/metrics"
-
-
-class MetricNotFoundWarning(UserWarning):
-    """Metric not found"""
 
 
 def get_metrics(context):
@@ -46,5 +40,9 @@ def get_counter(context, metric_name: str, metrics=None, *, labels=None):
                 return sum(x.value for x in family.samples)
             raise ValueError(f"Metric not supported: {family.type}")
 
-    warnings.warn(f"Metric {metric_name} not found", MetricNotFoundWarning)
+    # XXX: If the first request we send to the API is a metrics request, then the metrics
+    # are not yet available. We should probably check for the length of the response
+    # (the first request will return less metrics).
+    names = [x.name for x in metrics]
+    print(f"Warning: metric {metric_name!r} not found (metrics: {names})")
     return 0
