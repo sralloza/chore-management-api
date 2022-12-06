51e16c45af730697d90b46bc3b8997a99cbf8a34
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-12-07 00:20:46 +0100

    chore: add retry control in init_db script

--- utils/init_database.py
+++ utils/init_database.py
@@ -1,7 +1,18 @@
 import sys
 from pathlib import Path
+from time import sleep
+
+from pymysql.err import OperationalError
 
 sys.path.insert(0, Path(__file__).parent.parent.as_posix())
 from app.db.init_database import init_db_sync
 
-init_db_sync()
+if __name__ == "__main__":
+    for _ in range(10):
+        try:
+            init_db_sync()
+            break
+        except OperationalError as e:
+            print(e)
+            sleep(1)
+            continue
