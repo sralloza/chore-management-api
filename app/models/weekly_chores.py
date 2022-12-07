120708ff3a17438fdb979f2c2ab214a44f0e7d1d
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-12-07 21:16:52 +0100

    lint

--- app/models/weekly_chores.py
+++ app/models/weekly_chores.py
@@ -3,11 +3,12 @@ from pydantic import BaseModel, Field
 
 class WeeklyChore(BaseModel):
     assigned_ids: list[str] = Field(
-        description="List of user IDs assigned to the chore",example=["user-1","user-2"]
+        description="List of user IDs assigned to the chore",
+        example=["user-1", "user-2"],
     )
     assigned_usernames: list[str] = Field(
         description="List of usernames assigned to the chore",
-        example=["username-1","username-2"]
+        example=["username-1", "username-2"],
     )
     done: bool = Field(description="Whether the chore has been completed", example=True)
     type: str = Field(description="Chore type identifier", example="clean-dishes")
