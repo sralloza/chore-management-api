9c0ef99ee42d7a888432d41e305c761c0ebe2b56
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-12-30 13:50:48 +0100

    feat: add error translations for editSystemSettings operation

--- app/middlewares/errors.py
+++ app/middlewares/errors.py
@@ -1,5 +1,6 @@
 import logging
 
+import i18n
 from fastapi import Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
@@ -28,11 +29,11 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
     for error in exc.errors():
         if error["type"] == "value_error.jsondecode":
+            lang = request.headers.get("Accept-Language", "en")
+            detail = i18n.t("generic.request_invalid_json", locale=lang)
             return ORJSONResponse(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                content=jsonable_encoder(
-                    {"detail": "Request body is not a valid JSON"}
-                ),
+                content=jsonable_encoder({"detail": detail}),
                 headers=headers,
             )
     return ORJSONResponse(
